8d15555132f25db4817350823102c46c01768132
Vegar Ringdal <vegar.ringdal@gmail.com>
2018-12-25 12:30:01 -0800

    split file & db columns into enum

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,10 @@ import { generateTable, createSqlite, importData } from './dbUtils';
 
 
 const tablename = 'tags';
-const sqlLiteFilenameAndPath = './mydb.sqlite'
-const dataFilenameAndPath = './data.csv'
+const sqlLiteFilenameAndPath = './mydb.sqlite';
+const dataFilenameAndPath = './data.csv';
 
 const run = async () => {
-    
-
 
     const knex = createSqlite(sqlLiteFilenameAndPath);
     await generateTable(knex, tablename);
