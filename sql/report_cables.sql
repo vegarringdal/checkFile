cbca4b616720504ff5ca60872b7dd85fbaa70291
Vegar Ringdal <vegar.ringdal@gmail.com>
2019-01-03 19:04:24 +0100

    improve all errors sheet

--- sql/report_cables.sql
+++ sql/report_cables.sql
@@ -1,5 +1,40 @@
+WITH RECURSIVE 
+--------------------------
+not_d_tags as (
 select
     tag_no as Tag_no,
+    CASE
+    when 
+        (tag_cabletype is null
+        or
+        tag_from_tag is null
+        or
+        tag_to_tag is null
+        or
+        tag_segregation is null
+        or
+        tag_discipline is null
+        or
+        tag_eng_code is null
+        or
+        tag_eng_code = 'E'
+        or
+        tag_contractor is null
+        or (
+            (tag_cable_length is null or tag_cable_length = '0')
+            and
+            (tag_eng_code = 'A' or tag_eng_code = 'B')
+        )
+        or (
+            Tag_discipline != 'E' 
+            and 
+            Tag_discipline != 'I' 
+            and 
+            Tag_discipline != 'T'
+            )
+        ) then 'YES-[todo]'
+        else 'NO'
+        end as Errors,
     ifnull(tag_from_tag, '[todo]') as Tag_from_tag,
     ifnull(tag_to_tag, '[todo]') as Tag_to_tag,
     ifnull(tag_cabletype, '[todo]') as Tag_cabletype,
@@ -15,7 +50,7 @@ select
   		END as Tag_eng_code,
     ifnull(tag_contractor, 'ALL') as Tag_contractor,
     CASE 
-        when tag_cable_length  is null then '[todo]'
+        when tag_cable_length  is null and (tag_eng_code = 'A' or tag_eng_code = 'B') then '[todo]'
 		WHEN (tag_cable_length = "0" and (tag_eng_code = 'A' or tag_eng_code = 'B')) THEN '0-[todo]'
   		ELSE tag_cable_length
   	    END as Tag_cable_length,
@@ -26,7 +61,7 @@ from
     tags
 where
         tag_eng_code <> 'D'
-    and (
+    /* and (
         tag_cabletype is null
         or
         tag_from_tag is null
@@ -48,3 +83,64 @@ where
             (tag_eng_code = 'A' or tag_eng_code = 'B')
         )
     )
+ */
+),
+--------------------------
+d_tags as (
+select
+    tag_no as Tag_no,
+    'NO-[TBA]' as Errors,
+    ifnull(tag_from_tag, '[TBA]') as Tag_from_tag,
+    ifnull(tag_to_tag, '[TBA]') as Tag_to_tag,
+    ifnull(tag_cabletype, '[TBA]') as Tag_cabletype,
+    ifnull(tag_segregation, '[TBA]') as Tag_segregation,
+    CASE 
+        when Tag_discipline  is null then '[TBA]'
+		WHEN (Tag_discipline != 'E' and Tag_discipline != 'I' and Tag_discipline != 'T') THEN Tag_discipline||'-[TBA]'
+  		ELSE Tag_discipline
+  		END as Tag_discipline,
+    CASE (tag_eng_code)
+		WHEN "E" THEN "E-[TBA]"
+  		ELSE tag_eng_code
+  		END as Tag_eng_code,
+    ifnull(tag_contractor, 'ALL') as Tag_contractor,
+    CASE 
+        when tag_cable_length  is null then '[TBA]'
+		WHEN (tag_cable_length = "0" and (tag_eng_code = 'A' or tag_eng_code = 'B')) THEN '0-[TBA]'
+  		ELSE tag_cable_length
+  	    END as Tag_cable_length,
+    ifnull(tag_status, '') as Tag_status,
+    ifnull(tag_description, '') as Tag_description,
+    ifnull(tag_remark, '') as Tag_remark
+from
+    tags
+where
+        tag_eng_code = 'D'
+    /* and (
+        tag_cabletype is null
+        or
+        tag_from_tag is null
+        or
+        tag_to_tag is null
+        or
+        tag_segregation is null
+        or
+        tag_discipline is null
+        or
+        tag_eng_code is null
+        or
+        tag_eng_code = 'E'
+        or
+        tag_contractor is null
+        or (
+            (tag_cable_length is null or tag_cable_length = '0')
+            and
+            (tag_eng_code = 'A' or tag_eng_code = 'B')
+        )
+    )
+ */
+)
+
+select * from not_d_tags
+union
+select * from d_tags
