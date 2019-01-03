c3be1c93c2b71af730a6b9faee5ccdb47df9597f
Vegar Ringdal <vegar.ringdal@gmail.com>
2019-01-03 12:29:35 +0100

    Update cabletypes_BE.sql

--- sql/cabletypes_BE.sql
+++ sql/cabletypes_BE.sql
@@ -1,15 +1,14 @@
 select 
-		'BE code A,B' as Query_Info,
+		ifnull(tag_contractor, "undefined") as Tag_contractor,
 		ifnull(tag_cabletype, 'missing_type') || '-cables' as Cable_type,
 		sum(cast(tag_cable_length as real)) as Total_meters,
 		'NA' as Target
 	from 
 		tags
 	where 
-		tag_contractor = 'BE' 
-	and 
 		( tag_eng_code = 'A' 
 		or 
 		tag_eng_code = 'B')
 	GROUP by 
+		tag_contractor,
 		tag_cabletype
