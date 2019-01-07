dddd7691aeec3efd3d41c6b76b1404966a899c9e
Vegar Ringdal <vegar.ringdal@aibel.com>
2019-01-07 06:37:05 +0100

    reorder columns cable summary

--- sql/cabletypes.sql
+++ sql/cabletypes.sql
@@ -2,9 +2,6 @@ select
 	ifnull(tag_contractor, 'undefined') as Contractor,
 	ifnull(tag_cabletype, 'missing_type') as Cable_type,
 	count(*) as Cables,
-	ifnull(ROUND(MIN(cast(ifnull(tag_cable_length, 0) as real)) ,3), 0) AS 'Min[m]',
-	ifnull(ROUND(AVG(cast(ifnull(tag_cable_length, 0) as real)) ,3), 0) AS 'Avg[m]',
-	ifnull(ROUND(MAX(cast(ifnull(tag_cable_length, 0) as real)) ,3), 0) AS 'Max[m]',
 	ifnull(sum(cast(tag_cable_length as real)), 0) as 'Total[m]',
 	(
 		select 
@@ -29,7 +26,10 @@ select
 				or 
 				c.tag_cable_length = '0'
 			))
-			as 'Missing[m]'
+			as 'Missing[m]',
+	ifnull(ROUND(MIN(cast(ifnull(tag_cable_length, 0) as real)) ,3), 0) AS 'Min[m]',
+	ifnull(ROUND(AVG(cast(ifnull(tag_cable_length, 0) as real)) ,3), 0) AS 'Avg[m]',
+	ifnull(ROUND(MAX(cast(ifnull(tag_cable_length, 0) as real)) ,3), 0) AS 'Max[m]'
 from 
 	tags as b
 where 
