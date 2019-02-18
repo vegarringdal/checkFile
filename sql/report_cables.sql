038bce384b64956eebaebc6ac23697669c953503
Vegar Ringdal <vegar.ringdal@aibel.com>
2019-02-18 13:29:43 +0100

    fix engineering code = null

--- sql/report_cables.sql
+++ sql/report_cables.sql
@@ -61,30 +61,7 @@ select
 from
     tags
 where
-        tag_eng_code <> 'D'
-    /* and (
-        tag_cabletype is null
-        or
-        tag_from_tag is null
-        or
-        tag_to_tag is null
-        or
-        tag_segregation is null
-        or
-        tag_discipline is null
-        or
-        tag_eng_code is null
-        or
-        tag_eng_code = 'E'
-        or
-        tag_contractor is null
-        or (
-            (tag_cable_length is null or tag_cable_length = '0')
-            and
-            (tag_eng_code = 'A' or tag_eng_code = 'B')
-        )
-    )
- */
+        tag_eng_code <> 'D' and tag_no is not null
 ),
 --------------------------
 d_tags as (
@@ -117,32 +94,42 @@ select
 from
     tags
 where
-        tag_eng_code = 'D'
-    /* and (
-        tag_cabletype is null
-        or
-        tag_from_tag is null
-        or
-        tag_to_tag is null
-        or
-        tag_segregation is null
-        or
-        tag_discipline is null
-        or
+        tag_eng_code = 'D' and tag_no is not null
+),
+--------------------------
+null_tags as (
+select
+    tag_no as Tag_no,
+    'TJA' as Errors,
+    ifnull(tag_from_tag, '[TBA]') as Tag_from_tag,
+    ifnull(tag_to_tag, '[TBA]') as Tag_to_tag,
+    ifnull(tag_cabletype, '[TBA]') as Tag_cabletype,
+    ifnull(tag_segregation, '[TBA]') as Tag_segregation,
+    CASE 
+        when Tag_discipline  is null then '[TBA]'
+		WHEN (Tag_discipline != 'E' and Tag_discipline != 'I' and Tag_discipline != 'T') THEN Tag_discipline||'-[TBA]'
+  		ELSE Tag_discipline
+  		END as Tag_discipline,
+    ifnull(tag_eng_code, '[TODO]') as Tag_eng_code,
+    ifnull(tag_contractor, 'ALL') as Tag_contractor,
+    CASE 
+        when tag_cable_length  is null then '[TBA]'
+		WHEN (tag_cable_length = "0" and (tag_eng_code = 'A' or tag_eng_code = 'B')) THEN '0-[TBA]'
+  		ELSE tag_cable_length
+  	    END as Tag_cable_length,
+    ifnull(tag_installer_contractor, '') as Tag_installer_contractor, 
+    ifnull(tag_status, '') as Tag_status,
+    ifnull(tag_description, '') as Tag_description,
+    ifnull(tag_remark, '') as Tag_remark
+from
+    tags
+where
         tag_eng_code is null
-        or
-        tag_eng_code = 'E'
-        or
-        tag_contractor is null
-        or (
-            (tag_cable_length is null or tag_cable_length = '0')
-            and
-            (tag_eng_code = 'A' or tag_eng_code = 'B')
-        )
-    )
- */
+        and tag_no is not null
 )
 
 select * from not_d_tags
+union
+select * from null_tags
 union
 select * from d_tags
