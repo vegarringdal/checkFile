d6a2bfaefef5fd6d255d7b7fab30f24a3505326b
Vegar Ringdal <vegar.ringdal@aibel.com>
2019-04-25 13:48:13 +0200

    fix summary

--- sql/status.sql
+++ sql/status.sql
@@ -48,7 +48,7 @@ be_tags_sub_Con as(
 		'03. '|| ifnull(tag_contractor, 'Contractor undefined') || ' - Eng Code ' || ifnull(tag_eng_code, 'undefined'),
 		count(tag_no) as No,
 		CASE
-		WHEN tag_eng_code = "E" or tag_eng_code = "C" or tag_contractor is null THEN "ERRORS"
+		WHEN tag_eng_code = "E" or tag_eng_code = "C" or tag_contractor is null THEN "Must be corrected"
   		ELSE "INFO"
   		END,
 		ifnull(tag_contractor, 'All') as Tag_contractor,
@@ -95,7 +95,7 @@ be_tags_sub_Dis as(
 		'04. '|| ifnull(tag_contractor, 'Contractor undefined') || '- Eng Code: A, B - - Disc: ' || ifnull(tag_discipline, 'undefined'),
 		count(tag_no) as No,
 		CASE 
-		WHEN (tag_discipline != "E" and tag_discipline != "I" and tag_discipline != "T") or tag_contractor is null or tag_discipline is null THEN "ERRORS"
+		WHEN (tag_discipline != "E" and tag_discipline != "I" and tag_discipline != "T") or tag_contractor is null or tag_discipline is null THEN "Must be corrected"
   		ELSE "INFO"
   		END,
 		ifnull(tag_contractor, 'All') as Tag_contractor,
@@ -116,7 +116,7 @@ be_tags_from_tag as(
 	select 
 		'05. '|| ifnull(tag_contractor, 'Contractor undefined') || ' - From tag missing - Eng Code: A, B - Disc.: '|| ifnull(tag_discipline, '?'),
 		count(tag_no),
-		'ERRORS' as target,
+		'Must be corrected' as target,
 		ifnull(tag_contractor, 'All') as Tag_contractor,
 		ifnull(tag_discipline, 'All') as Discipline,
 		'From tag missing - Eng Code A, B' as Groups,
@@ -136,7 +136,7 @@ be_tags_to_tag as(
 	select 
 		'06. '|| ifnull(tag_contractor, 'Contractor undefined') || ' - To tag missing - Eng Code: A, B - Disc.: '|| ifnull(tag_discipline, '?'),
 		count(tag_no),
-		'ERRORS' as target,
+		'Must be corrected' as target,
 		ifnull(tag_contractor, 'All') as Tag_contractor,
 		ifnull(tag_discipline, 'All') as Discipline,
 		'To tag missing  - Eng Code A, B' as Groups,
@@ -157,7 +157,7 @@ be_tags_segreation as(
 	select 
 		'07. '|| ifnull(tag_contractor, 'Contractor undefined') || ' - Segregation missing - Eng Code: A, B - Disc.: '|| ifnull(tag_discipline, '?'),
 		count(tag_no),
-		'ERRORS' as target,
+		'Must be corrected' as target,
 		ifnull(tag_contractor, 'All') as Tag_contractor,
 		ifnull(tag_discipline, 'All') as Discipline,
 		'Segregation missing - Eng Code A, B' as Groups,
@@ -178,7 +178,7 @@ be_tags_cable1 as(
 	select 
 		'08. '|| ifnull(tag_contractor, 'Contractor undefined') || ' - Cabletype STID missing - Eng Code: A, B - Disc.: '|| ifnull(tag_discipline, '?'),
 		count(tag_no),
-		'ERRORS' as target,
+		'Must be corrected' as target,
 		ifnull(tag_contractor, 'All') as Tag_contractor,
 		ifnull(tag_discipline, 'All') as Discipline,
 		'Cabletype STID missing - Eng Code A, B' as Groups,
@@ -199,7 +199,7 @@ be_tags_cable1 as(
 --	select 
 ----		'09. '|| ifnull(tag_contractor, 'Contractor undefined') || ' - Cabletype STID missing - Eng Code: A, B - Disc.: '|| ifnull(tag_discipline, '?'),
 --		count(tag_no),
---		'ERRORS' as target,
+--		'Must be corrected' as target,
 --		ifnull(tag_contractor, 'All') as Tag_contractor,
 --		ifnull(tag_discipline, 'All') as Discipline,
 --		'Cabletype STID missing - Eng Code A, B' as Groups,
@@ -220,7 +220,7 @@ be_tags_disiplin1 as(
 	select 
 		'10. '|| ifnull(tag_contractor, 'Contractor undefined') || ' - Discipline missing - Eng Code: A, B',
 		count(tag_no),
-		'ERRORS' as target,
+		'Must be corrected' as target,
 		ifnull(tag_contractor, 'All') as Tag_contractor,
 		ifnull(tag_discipline, 'All') as Discipline,
 		'Discipline missing - Eng Code A, B' as Groups,
@@ -241,12 +241,12 @@ be_tags_routingstatus as(
 		'13. '|| ifnull(tag_contractor, 'Contractor undefined') || ' - Cable ' || ifnull(tag_cable_status, 'undefined') || ' - Eng Code: A, B - Disc.: '|| ifnull(tag_discipline, '?'),
 		count(tag_no) as No,
 		CASE 
-		WHEN (tag_cable_status != "RE") or tag_contractor is null or tag_cable_status is null THEN "ERRORS"
+		WHEN (tag_cable_status != "RE" and tag_cable_status != "CR" and tag_cable_status != "RO") or tag_contractor is null or tag_cable_status is null THEN "Must be corrected"
   		ELSE "INFO"
   		END,
 		ifnull(tag_contractor, 'All') as Tag_contractor,
 		ifnull(tag_discipline, 'All') as Discipline,
-		'Cable status missing - Eng Code A, B' as Groups,
+		'Cable status - Eng Code A, B' as Groups,
 		'' as 'Last Week',
 		'' as 'Comment meeting'
 	from 
@@ -263,7 +263,7 @@ be_tags_contractinstaller as(
 		'16. '|| ifnull(tag_contractor, 'Contractor undefined') || ' - Installer ' || ifnull(tag_installer_contractor, 'undefined') || ' - Eng Code: A, B - Disc.: '|| ifnull(tag_discipline, '?'),
 		count(tag_no) as No,
 		CASE 
-		WHEN tag_contractor is null or tag_installer_contractor is null THEN "ERRORS"
+		WHEN tag_contractor is null or tag_installer_contractor is null THEN "Must be corrected"
   		ELSE "INFO"
   		END,
 		ifnull(tag_contractor, 'All') as Tag_contractor,
@@ -284,7 +284,7 @@ be_tags_PO as(
 	select 
 		'14. '|| ifnull(tag_contractor, 'Contractor undefined') || ' - PO missing - Eng Code: A, B - Disc.: '|| ifnull(tag_discipline, '?'),
 		count(tag_no),
-		'ERRORS' as target,
+		'Must be corrected' as target,
 		ifnull(tag_contractor, 'All') as Tag_contractor,
 		ifnull(tag_discipline, 'All') as Discipline,
 		'PO missing  - Eng Code A, B' as Groups,
@@ -305,7 +305,7 @@ be_tags_system as(
 	select 
 		'15. '|| ifnull(tag_contractor, 'Contractor undefined') || ' - Tag system missing- Eng Code: A, B - Disc.: '|| ifnull(tag_discipline, '?'),
 		count(tag_no),
-		'ERRORS' as target,
+		'Must be corrected' as target,
 		ifnull(tag_contractor, 'All') as Tag_contractor,
 		ifnull(tag_discipline, 'All') as Discipline,
 		'Tag system missing  - Eng Code A, B' as Groups,
@@ -328,7 +328,7 @@ be_tags_length1 as(
 		count(tag_no),
 		case 
 		when tag_contractor is null OR tag_discipline is null 
-		OR tag_cable_length = '0' OR tag_cable_length = 0 or tag_cable_length is null then 'ERRORS'
+		OR tag_cable_length = '0' OR tag_cable_length = 0 or tag_cable_length is null then 'Must be corrected'
 		else 'INFO'
 		end AS TARGET,
 		ifnull(tag_contractor, 'All') as Tag_contractor,
@@ -354,7 +354,7 @@ be_tags_length2 as(
 		'12. '|| ifnull(tag_contractor, 'Contractor undefined') || ' - Eng Code: A, B - - Disc.: '|| ifnull(tag_discipline, '?') || ' - Total length [m]',
 		sum(ifnull(cast(tag_cable_length as real), 0)),
 		case 
-		when tag_contractor is null OR tag_discipline is null then 'ERRORS'
+		when tag_contractor is null OR tag_discipline is null then 'Must be corrected'
 		else 'INFO'
 		end AS TARGET,
 		ifnull(tag_contractor, 'All') as Tag_contractor,
